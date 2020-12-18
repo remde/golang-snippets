10518da0de6c2110e16c2df54c6a911f90becdd3
Fernando Remde <ffremde@gmail.com>
2020-12-18 18:50:33 -0300

    first commit

---
+++ echo/echo1.go
@@ -0,0 +1,13 @@
+//Echo1 prints its command-line arguments along with the index
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	for i := 1; i < len(os.Args); i++ {
+		fmt.Println(i, os.Args[i])
+	}
+}
---
+++ hello-world/helloworld.go
@@ -0,0 +1,8 @@
+// helloworld prints "Hello, World"
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, world")
+}
